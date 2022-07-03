014e0be57948c3936fd7336ea2ea9526fde7cac1
Nathan <nslee333@gmail.com>
2022-07-03 12:39:20 -0700

    Commit

--- JavaScript/BasicAlgorithms/Finders.js
+++ JavaScript/BasicAlgorithms/Finders.js
@@ -24,7 +24,12 @@ console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 
 
         This function takes in an array and a function, and returns the first element of the array that passes the function test.
 
+        The only piece of code that changes the for loop from finding the largest last element and the first element that passes the function, is the
+            return keyword, which tells the loop that once we find the an element that passes, we end the loop.
+
         If there is no element that passes the function test, then return undefined.
+
+
 
 
     */
