b4a6aa6013ebe725bf591d682d267392419b8ec8
Nathan <nslee333@gmail.com>
2022-07-09 12:28:23 -0700

    Commit

---
+++ JavaScript/Basic JS/Misc/FunctionFormatsClean.js
@@ -0,0 +1,23 @@
+
+
+function traditionalFunction() {
+    console.log("Hello World!");
+}
+
+
+traditionalFunction();
+
+
+const arrowFunction = function() {
+    console.log("Hello World!");
+}
+
+
+const arrowFunctionV2 = () => {
+    console.log("Hello World!");
+}
+
+
+
+const arrowFunctionV3 = () => "value";
+
