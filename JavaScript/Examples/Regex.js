3769174179bdbb9826728ff40b0dce6f3bc08b41
Nathan <nslee333@gmail.com>
2022-07-13 16:24:29 -0700

    Commit.

--- JavaScript/Examples/Regex.js
+++ JavaScript/Examples/Regex.js
@@ -1,3 +1,32 @@
 let testStr = "freeCodeCamp";
 let testRegex = /Code/;
 testRegex.test(testStr); 
+
+"Hello, world!".match(/Hello/);
+let ourStr = "Regular expressions";
+let ourRegex = /expressions/;
+ourStr.match(ourRegex);
+
+
+let testStr2 = "Repeat, Repeat, Repeat";
+let ourRegex2 = /Repeat/;
+testStr.match(ourRegex);
+
+
+let repeatRegex = /Repeat/g;
+testStr.match.match(repeatRegex);
+
+
+let soccerWord = "goooooooooal";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex);
+gPhrase.match(goRegex);
+oPhrase.match(goRegex);
+
+let firstString = "Ricky is the first and can be found";
+let firstRegex = /^Ricky/;
+firstRegex.test(firstString);
+let notFirst = "You can't find Ricky now.";
+firstRegex.test(notFirst);
