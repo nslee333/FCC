a2a231950e1721a24de8f053ebde778394176aa1
Nathan <nslee333@gmail.com>
2022-07-13 20:23:18 -0700

    Commit

--- JavaScript/IntermediateAlgorithms/MissingLetters.js
+++ JavaScript/IntermediateAlgorithms/MissingLetters.js
@@ -20,7 +20,12 @@ function fearNotLetter(str) {
 
   /*
   
-  
+  This function takes in a string, and returns the letter missing from that string in alphabetical order.
+
+  For each element in the string:
+    - Let code be the char code of the current iteration.
+    - If code does not equal the char code of the first position plus iterations,
+        return 
 
 
   
