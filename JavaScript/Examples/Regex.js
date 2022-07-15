680b4abdd8e616826696ffaf2ff8eeb7486d3e16
Nathan <nslee333@gmail.com>
2022-07-14 18:49:49 -0700

    Commit

--- JavaScript/Examples/Regex.js
+++ JavaScript/Examples/Regex.js
@@ -30,3 +30,35 @@ let firstRegex = /^Ricky/;
 firstRegex.test(firstString);
 let notFirst = "You can't find Ricky now.";
 firstRegex.test(notFirst);
+
+let theEnding = "This is a never ending story";
+let storyRegex = /story&/;
+storyRegex.test(theEnding);
+let noEnding = "Sometimes a story will have to end";
+storyRegex.test(noEnding);
+
+
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+multipleA.test(A4);
+multipleA.test(A2);
+
+
+let american = "color";
+let british = "colour";
+let rainbowRegex = /colou?r/;
+rainbowRegex.test(american);
+rainbowRegex.test(british);
+
+let quit = "qu";
+let noquit = "qt";
+let quRegex = /q(?=u)/;
+let qRegex = /q(?!u)/;
+quit.match(quRegex);
+noquit.match(qRegex);
+
+let password = "abc123";
+let checkPass = /(?=\w{3,6})(?=\D*\d)/;
+
+let repeatStr = "row row row your boat";
