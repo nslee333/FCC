0700de45985a75d3e7bded872e6189d76645dc44
Nathan <nslee333@gmail.com>
2022-07-18 18:06:01 -0700

    Commit

---
+++ JavaScript/IntermediateAlgorithms/13SumAllPrimes.js
@@ -0,0 +1,28 @@
+function sumPrimes(num) {
+
+    function isPrime(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) 
+          return false;
+    }
+          return true;
+        }
+          
+    
+    
+    
+    
+    let result = 0;
+    
+    for(let i = 2; i <= num; i++) {
+      if(isPrime(i)) {
+        result += i;
+      }
+    }
+      return result;
+    }
+    
+    console.log(sumPrimes(10));
+
+
+    
