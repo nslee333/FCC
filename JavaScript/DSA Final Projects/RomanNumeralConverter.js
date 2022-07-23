a2f75a60115e2ae0e9bfb6e379a1e700cd3d9eb6
Nathan <nslee333@gmail.com>
2022-07-23 08:20:05 -0700

    Commit

---
+++ JavaScript/DSA Final Projects/RomanNumeralConverter.js
@@ -0,0 +1,43 @@
+function helperI(string) {
+    if(string === "1") {
+      return "I";
+    } else if (string === "2") {
+      return "II";
+    } else if (string === "3") {
+      return "III";
+    } else if (string === "4") {
+      return "IV";
+    } else if (string === "5"){
+      return "V";
+    } else if (string === "6"){
+      return "VI";
+    } else if (string === "7"){
+      return "VII";
+    } else if (string === "8"){
+      return "VIII";
+    } else if (string === "9"){
+      return "IX";
+    }
+  function helper2
+
+
+
+console.log(string, string.length - 1);
+  
+
+  let resultString = "";
+  
+  if(num > 1000) {
+    string += "M";
+  } else if (num > 900) {
+    string += "CM";
+  } else if (num > 500)
+
+
+  return string;
+  
+  
+  
+  for(let i = string.length -1 ; i > 0; i-- ) {
+
+}
